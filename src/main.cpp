f703e1f2bad275651c173150bdb73d04b1c8f69d
umbertofrega <umbertofrega@gmail.com>
2026-06-13 13:29:01 +0200

    integration of wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <Arduino.h>
+#include "network_manager.h"
+
+NetworkManager networkManager;
 
 void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  pinMode(LED_GREEN, OUTPUT);
+
+
+  if (networkManager.connectToWifi()){
+    digitalWrite(LED_GREEN, LOW);
+  } 
 }
 
 void loop() {}
