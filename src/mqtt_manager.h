fd1d45341b0e09f9eca1e96d4c1c22b2625e752f
umbertofrega <umbertofrega@gmail.com>
2026-06-13 17:02:15 +0200

    added mqtt_manager logic

---
+++ src/mqtt_manager.h
@@ -0,0 +1,44 @@
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+class MqttManager
+{
+private:
+    const char *mqtt_broker = "192.168.1.102";
+    const int mqtt_port = 1883;
+
+    WiFiClient wifiClient;
+    PubSubClient mqttClient;
+
+public:
+    MqttManager() : mqttClient(wifiClient)
+    {
+        mqttClient.setServer(mqtt_broker, mqtt_port);
+    }
+
+    bool connectToBroker()
+    {
+        Serial.println("MQTT Connection Started");
+
+        if (mqttClient.connect("esp32tesi"))
+        {
+            Serial.println("Connected to Broker Successfully");
+
+            mqttClient.publish("esp32/connection", "Connected");
+            return true;
+        }
+
+        Serial.println("Connection Failed");
+        return false;
+    }
+
+    void loop()
+    {
+        mqttClient.loop();
+    }
+
+    bool isConnected()
+    {
+        return mqttClient.connected();
+    }
+};
