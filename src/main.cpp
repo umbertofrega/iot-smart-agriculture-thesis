e093c56ae9f52b4747dcb05f66ed18fc57459ba2
umbertofrega <umbertofrega@gmail.com>
2026-06-13 17:02:43 +0200

    implemented the mqtt manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,45 @@
 #include <Arduino.h>
 #include "network_manager.h"
+#include "mqtt_manager.h"
 
 NetworkManager networkManager;
+MqttManager mqttManager;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(2000);
   pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
 
-
-  if (networkManager.connectToWifi()){
+  if (networkManager.connectToWifi())
+  {
     digitalWrite(LED_GREEN, LOW);
-  } 
+  }
+
+  if (mqttManager.connectToBroker())
+  {
+    digitalWrite(LED_BLUE, LOW);
+  }
 }
 
-void loop() {}
+void loop()
+{
+  if (!networkManager.isConnected())
+  {
+    digitalWrite(LED_GREEN, HIGH);
+  }
+
+  if (!mqttManager.isConnected())
+  {
+    digitalWrite(LED_BLUE, HIGH);
+    if (mqttManager.connectToBroker())
+
+    {
+      digitalWrite(LED_BLUE, LOW);
+    }
+  }
+
+  mqttManager.loop();
+}
