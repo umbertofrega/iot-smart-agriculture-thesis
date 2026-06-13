33db02a0f58a0703c581835eedc59ad3c14c5cb5
umbertofrega <umbertofrega@gmail.com>
2026-06-13 13:14:46 +0200

    added network manager

--- src/network_manager.h
+++ src/network_manager.h
@@ -1,1 +1,30 @@
+#include <WiFi.h>
+#include <secrets.h>
 
+class NetworkManager{
+
+     boolean connectToWifi(){
+        int tries = 20;
+
+        WiFi.begin(ssid,passwd);
+        Serial.println("Connecting to WiFi");
+
+        while(WiFi.status() != WL_CONNECTED && tries > 0){
+            tries--;
+            delay(500);
+        }
+        
+        if(WiFi.status() == WL_CONNECTED){
+            Serial.print("Connection successful!");
+            return true;
+        } else {
+            Serial.print("Connection failed");
+            return false;
+        }
+    }
+
+    boolean isConnected(){
+        return WiFi.status() != WL_CONNECTED;
+    }
+
+};
