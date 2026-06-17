f7d2c5a656a3b029720675bed0408056c32d32ca
umbertofrega <umbertofrega@gmail.com>
2026-06-17 14:55:08 +0200

    implemented temperature sensor in manager

--- src/sensors/sensors_manager.h
+++ src/sensors/sensors_manager.h
@@ -1,7 +1,16 @@
-
+#include "temperature_sensor.h"
 
 class SensorsManager
 {
+private:
+    float temperature;
+    TemperatureSensor temperatureSensor{PIN_DAC1};
+
 public:
-    float *getData();
+    float *getData()
+    {
+        float temperature = temperatureSensor.readData();
+        Serial.println("The temperature is: ");
+        Serial.println(temperature);
+    }
 };
