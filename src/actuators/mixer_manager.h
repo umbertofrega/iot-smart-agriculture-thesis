26a01500034f7634ce6da6208bd851228a98fe4e
umbertofrega <umbertofrega@gmail.com>
2026-06-25 16:28:19 +0200

    better mixers logic

--- src/actuators/mixer_manager.h
+++ src/actuators/mixer_manager.h
@@ -6,22 +6,26 @@ class MixersManager
 private:
     Mixer acidicMixer;
     Mixer basicMixer;
+    int pinAcidic;
+    int pinBasic;
 
 public:
-    MixersManager(int pinAcidic, int pinBasic)
+    MixersManager(int pinA, int pinB)
     {
-        acidicMixer = Mixer(pinAcidic);
-        basicMixer = Mixer(pinBasic);
+        pinAcidic = pinA;
+        pinBasic = pinB;
     }
 
     void mixBasic()
     {
+        acidicMixer = Mixer(pinBasic);
         basicMixer.mix();
         Serial.println("Mixed basic");
     }
 
     void mixAcidic()
     {
+        basicMixer = Mixer(pinAcidic);
         acidicMixer.mix();
         Serial.println("Mixed acidic");
     }
