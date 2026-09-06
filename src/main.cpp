b53c794d25399fd9e2976feea6b01acf0dfaedbf
umbertofrega <umbertofrega@gmail.com>
2026-09-06 13:58:10 +0200

    made main a real usable prototype

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include <ArduinoJson.h>
 #include "sensors/sensors_manager.h"
 #include "network/network_manager.h"
+#include "actuators/mixer_manager.h"
 
 NetworkManager networkManager;
 SensorsManager sensorsManager;
+MixersManager mixersManager;
 
 void setup()
 {
@@ -17,13 +19,61 @@ void setup()
 
   JsonDocument data;
 
-  // float temp = sensorsManager.getTemp();
-  // float ph = sensorsManager.getPh(temp);
+  float temp = sensorsManager.getTemp();
+  float ph = sensorsManager.getPh(temp);
+  float hum = sensorsManager.getHumidityPercentage();
+
+  data["ph"] = ph;
+  data["temp"] = temp;
+  data["humidity"] = hum;
+
+  char sensorsBuffer[256];
+
+  char mixersBuffer[256];
+
+  serializeJson(data, sensorsBuffer);
+
+  serializeJsonPretty(data, Serial);
+
+  data.clear();
+
+  if (hum < 40)
+  {
+    data["irrigation"] = true;
+    if (ph > 6.5)
+    {
+      mixersManager.mixBasic();
+      data["mixed"] = "basic";
+    }
+    else if (ph < 4)
+    {
+      mixersManager.mixAcidic();
+      data["mixed"] = "acid";
+    }
+    else
+    {
+      data["mixed"] = "didn't mix";
+    }
+  }
+  else
+  {
+    data["irrigation"] = false;
+  }
+
+  serializeJson(data, mixersBuffer);
+
+  serializeJsonPretty(data, Serial);
+
+  if (networkManager.connect())
+  {
+    networkManager.publishSensors(sensorsBuffer);
+
+    networkManager.publishMixers(mixersBuffer);
+
+    networkManager.disconnect();
+  }
 }
 
 void loop()
 {
-  Serial.println(sensorsManager.getHumidityPercentage());
-
-  delay(2000);
 }
