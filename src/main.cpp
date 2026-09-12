c757cc68438bf44b61998619052b22eaf90569d5
umbertofrega <umbertofrega@gmail.com>
2026-09-12 19:00:40 +0200

    definitive deep sleep logic and main

--- src/main.cpp
+++ src/main.cpp
@@ -9,37 +9,54 @@ NetworkManager networkManager;
 SensorsManager sensorsManager;
 MixersManager mixersManager;
 
+const uint64_t TIME_TO_SLEEP_SEC = 60;
+
 void setup()
 {
   Serial.begin(115200);
   delay(2000);
+
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
 
+  esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+  if (cause == ESP_SLEEP_WAKEUP_TIMER)
+  {
+    Serial.println("\n[SISTEMA] Risveglio dal Deep Sleep.");
+  }
+  else
+  {
+    Serial.println("\n[SISTEMA] Avvio pulito o Reset.");
+  }
+
+  sensorsManager.beginAll();
+
+  float temp = sensorsManager.getTemp();
+  float ph = sensorsManager.getPh((int)temp);
+  int hum = sensorsManager.getHumidityPercentage();
+
+  Serial.printf("Letture -> Temp: %.2fC | pH: %.2f | Hum: %d%%\n", temp, ph, hum);
+
   JsonDocument data;
-
-  float temp = 34;
-  float ph = 5;
-  float hum = 30;
-
   data["ph"] = ph;
   data["temp"] = temp;
   data["humidity"] = hum;
 
   char sensorsBuffer[256];
-
   char mixersBuffer[256];
 
   serializeJson(data, sensorsBuffer);
-
   serializeJsonPretty(data, Serial);
+  Serial.println();
 
   data.clear();
 
   if (hum < 30)
   {
+    Serial.println("[ATTUATORI] Terreno secco. Calcolo mix...");
     data["irrigation"] = true;
+
     if (ph > 6.5)
     {
       mixersManager.mixBasic();
@@ -56,18 +73,31 @@ void setup()
     }
 
     serializeJson(data, mixersBuffer);
-
     serializeJsonPretty(data, Serial);
-  }
+    Serial.println();
 
-  if (networkManager.connect())
+    Serial.println("[NETWORK] Connessione WiFi/MQTT in corso...");
+    if (networkManager.connect())
+    {
+      networkManager.publishSensors(sensorsBuffer);
+      networkManager.publishMixers(mixersBuffer);
+      networkManager.disconnect();
+      Serial.println("[NETWORK] Dati inviati. Disconnesso.");
+    }
+    else
+    {
+      Serial.println("[NETWORK] Impossibile connettersi.");
+    }
+  }
+  else
   {
-    networkManager.publishSensors(sensorsBuffer);
-
-    networkManager.publishMixers(mixersBuffer);
-
-    networkManager.disconnect();
+    Serial.println("[ATTUATORI] Umidità OK. Salto le pompe e il WiFi.");
   }
+
+  Serial.printf("\n[SISTEMA] Vado in Deep Sleep per %llu secondi...\n", TIME_TO_SLEEP_SEC);
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP_SEC * 1000000ULL);
+  Serial.flush();
+  esp_deep_sleep_start();
 }
 
 void loop()
